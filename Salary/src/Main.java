a43fcb7dd23c0d389a9f3f3b9ab94d8c9f27e68a
Miytha-alzaabi <139710767+Miytha-alzaabi@users.noreply.github.com>
2023-08-05 15:03:28 +0400

    Add files via upload

---
+++ Salary/src/Main.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+class Employee {
+     String name;
+    double salary;
+
+     Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+}
+public class Main {
+    static List<Employee> employees = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1. Create Add Employee. \n 2. Show EmployeeDetails. \n 3. Exit");
+            System.out.print("Enter your Choice: ");
+            int choice = read.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter Employee Name: ");
+                    read.nextLine();
+                    String name = read.nextLine();
+                    System.out.print("Enter Employee Salary: ");
+                    double salary = read.nextDouble();
+                    employees.add(new Employee(name, salary));
+                    System.out.println("Employee Added Successfully.");
+                    break;
+                case 2:
+                    System.out.println("Employee salaries:");
+                    for (Employee employee : employees) {
+                        System.out.println(" Employee: "+employee.getName() + ": with " + employee.getSalary()+" OMR Salary");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please choose again.");
+            }
+        }
+    }
+}
